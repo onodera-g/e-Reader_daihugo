7831ad99769a020f092baaf0d32795dc35526c1e
onodera-g <ga19981023ku@gmail.com>
2025-09-17 10:01:38 +0000

    メッセージ管理用ファイルの追加

---
+++ include/messages_autogen.h
@@ -0,0 +1,14 @@
+#pragma once
+// Auto-generated from text/messages_jpn.txt (UTF-8 -> Shift_JIS). DO NOT EDIT.
+static const unsigned char MSG_CARD[] = "\x81\xA1";
+static const unsigned char MSG_CLEAR[] = "";
+static const unsigned char MSG_MARK[] = "\x82\x77";
+static const unsigned char MSG_TIME_LABEL[] = "\x82\x73\x82\x68\x82\x6C\x82\x64\x81\x46";
+static const unsigned char MSG_TARGET_LABEL[] = "\x82\xBD\x82\xA2\x82\xB5\x82\xE5\x82\xA4\x81\x46";
+static const unsigned char MSG_TARGET_TIMER[] = "\x83\x5E\x83\x43\x83\x7D\x81\x5B";
+static const unsigned char MSG_TARGET_MUSIC[] = "\x82\xA8\x82\xF1\x82\xAA\x82\xAD";
+static const unsigned char MSG_TARGET_BG[] = "\x82\xCD\x82\xA2\x82\xAF\x82\xA2";
+static const unsigned char MSG_MUSIC_LABEL[] = "\x82\xA8\x82\xF1\x82\xAA\x82\xAD\x81\x46";
+static const unsigned char MSG_BG_LABEL[] = "\x82\xCD\x82\xA2\x82\xAF\x82\xA2\x81\x46";
+static const unsigned char MSG_BG_SEP[] = "\x81\x5E";
+static const unsigned char MSG_CARD_TITLE[] = "\x44\x61\x69\x68\x75\x47\x6F";
